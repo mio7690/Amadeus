efa0f4e1a46dad4107d84bc7f9aecced62e7cb7c
mio <mio@lris.net>
2022-08-08 22:02:58 +0800

    fix config

--- code/vits-japanese/train.py
+++ code/vits-japanese/train.py
@@ -75,12 +75,12 @@ def run(rank, n_gpus, hps):
       rank=rank,
       shuffle=True)
   collate_fn = TextAudioCollate()
-  train_loader = DataLoader(train_dataset, num_workers=8, shuffle=False, pin_memory=True,
+  train_loader = DataLoader(train_dataset, num_workers=8, shuffle=False, pin_memory=False,
       collate_fn=collate_fn, batch_sampler=train_sampler)
   if rank == 0:
     eval_dataset = TextAudioLoader(hps.data.validation_files, hps.data)
     eval_loader = DataLoader(eval_dataset, num_workers=8, shuffle=False,
-        batch_size=hps.train.batch_size, pin_memory=True,
+        batch_size=hps.train.batch_size, pin_memory=False,
         drop_last=False, collate_fn=collate_fn)
 
   net_g = SynthesizerTrn(
