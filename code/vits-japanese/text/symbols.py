0eb9efda4652bd122a83995d5ad9d13e4902f387
mio <mio@lris.net>
2022-08-08 17:31:13 +0800

    add vit

---
+++ code/vits-japanese/text/symbols.py
@@ -0,0 +1,19 @@
+# symbols = list(' !"&*,-.?ABCINU[]abcdefghijklmnoprstuwyz{}~')
+# SPACE_ID = symbols.index(" ")
+
+""" from https://github.com/keithito/tacotron """
+
+'''
+Defines the set of symbols used in text input to the model.
+'''
+_pad        = '_'
+_punctuation = ',.!?-'
+_letters = 'AEINOQUabdefghijkmnoprstuvwyz'
+_letters_ipa = 'ʃʧ↓↑ '
+
+
+# Export all symbols:
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+
+# Special symbol ids
+SPACE_ID = symbols.index(" ")
